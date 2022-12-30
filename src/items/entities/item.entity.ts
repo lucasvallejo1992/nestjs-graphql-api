2add5f954003120d6051136fa173eee1256d2b3c
Lucas Vallejo <lucasvallejo92@gmail.com>
2022-12-30 19:52:27 -0300

    ✨ feat: add item entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,22 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field(() => String)
+  @Column()
+  name: string;
+
+  @Field(() => Float)
+  @Column()
+  quantity: number;
+
+  @Field(() => String)
+  @Column()
+  quantityUnits: string;
 }
