f46b1b39bc3267050c00cec9951b399643a29dc2
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-01-17 21:28:10 -0300

    ♻️ refactor: add auth guard to user resolver

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -3,7 +3,6 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum ValidRoles {
   admin = 'admin',
   user = 'user',
-  superUser = 'superUser',
 }
 
 registerEnumType(ValidRoles, { name: 'roles' });
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,27 +1,37 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { RolesArgs } from './dto/args/roles.args';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
   findAll(
-    @Args()
-    findAllArgs: RolesArgs,
+    @Args() findAllArgs: RolesArgs,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<User[]> {
     return this.usersService.findAll(findAllArgs.roles);
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => String }) id: string): Promise<User> {
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User> {
     return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
-  User(@Args('id', { type: () => String }) id: string): Promise<User> {
+  User(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+  ): Promise<User> {
     return this.usersService.remove(id);
   }
 }
